ffc3ba8c4d9b956fb453a920ee150e3688a60ccf
valentine-e <valia101112@gmail.com>
2023-10-16 00:44:39 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import ContactForm from './ContactForm/ContactForm';
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    // contacts: [],
+    // filter: '',
+    name: '',
+    number: '',
+  };
+
+  inputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  onSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <ContactForm
+          name={this.state.name}
+          number={this.state.number}
+          onSubmit={this.onSubmitHandler}
+          onChange={this.inputChange}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { nanoid } from 'nanoid';
+// import { Formik, Field, Form } from 'formik';
+
+const ContactForm = ({ name, number, onSubmit, onChange }) => {
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor={nameInputId}>
+        Name
+        <input
+          id={nameInputId}
+          type="text"
+          name="name"
+          value={name}
+          required
+          onChange={onChange}
+        />
+      </label>
+      <label htmlFor={numberInputId}>
+        Number
+        <input
+          id={numberInputId}
+          type="tel"
+          name="number"
+          value={number}
+          required
+          onChange={onChange}
+        />
+      </label>
+      <button>Add contact</button>
+      <ul>
+        <li>
+          <span>{name}</span> <span>{number}</span>
+        </li>
+      </ul>
+    </form>
+
+    // <Formik
+    //   initialValues={{
+    //     name: '',
+    //     number: '',
+    //     email: '',
+    //   }}
+    //   onSubmit={values => {
+    //     console.log(values);
+    //   }}
+    // >
+    //   <Form>
+    //     <label htmlFor="name">First Name</label>
+    //     <Field id="name" name="name" placeholder="Name" />
+
+    //     <label htmlFor="number">Last Name</label>
+    //     <Field id="number" name="number" placeholder="Number" />
+
+    //     <button type="submit">Submit</button>
+    //   </Form>
+    // </Formik>
+  );
+};
+
+export default ContactForm;
